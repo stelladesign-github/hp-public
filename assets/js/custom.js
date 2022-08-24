d95458241f2dce53d39fa9df828cfe04954c0196
stelladesign-github <stelladesign@st-bros.info>
2022-08-24 17:57:47 +0900

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,17 @@
-startFade($("#fadeImg1"), ["images/pic022.jpg", "images/pic02.jpg"]);
+startFade($("#fadeImg1"), ["images/pic02.jpg", "images/pic022.jpg"]);
 function startFade(img, urls) {
   img.addClass("fade");
   let count = 0;
   setInterval(function () {
-    if (count == urls.length) count = 0;
-    img.attr("src", urls[count]);
     count++;
+    if (count == urls.length) count = 0;
+    img.removeClass("fade");
+    img.css("opacity", 0);
+
+    setTimeout(function () {
+      img.addClass("fade");
+      img.css("opacity", 0);
+      img.attr("src", urls[count]);
+    }, 100);
   }, 5000);
 }
